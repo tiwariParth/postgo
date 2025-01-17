2c23a3796f0180f735c9c1f0c53ee5a7f7f0d3b0
tiwariParth <243mjparth@gmail.com>
2025-01-17 19:34:26 +0530

    refactor main application to include HTTP request handling and improve flag parsing

--- main.go
+++ main.go
@@ -1,29 +1,67 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
+    "flag"
+    "fmt"
+    "os"
+    "net/http"
+    "time"
 )
 
-type Config struct{
-	URL string
+type Config struct {
+    URL string
 }
-func parseFlags() (*Config, error){
-	config:= &Config{}
-	flag.StringVar(&config.URL, "url", "", "URL to send request to")
-	flag.Parse()
-	if config.URL == ""{
-		return nil, fmt.Errorf("Error: URL is required")
-	}
-	return config, nil
+
+func parseFlags() (*Config, error) {
+    config := &Config{}
+    flag.StringVar(&config.URL, "url", "", "URL to send request to")
+    flag.Parse() 
+    
+    if config.URL == "" {
+        return nil, fmt.Errorf("Error: URL is required")
+    }
+    return config, nil
 }
+
+func makeRequest(config *Config) error {
+    currentTime := time.Now().UTC().Format("2006-01-02 15:04:05")
+    fmt.Printf("Current Date and Time (UTC): %s\n", currentTime)
+    
+    currentUser := os.Getenv("USER")
+    fmt.Printf("Current User: %s\n\n", currentUser)
+
+    client := &http.Client{
+        Timeout: 10 * time.Second,
+    }
+
+    req, err := http.NewRequest("GET", config.URL, nil)
+    if err != nil {
+        return fmt.Errorf("Error creating request: %v", err)
+    }
+
+    resp, err := client.Do(req)
+    if err != nil {
+        return fmt.Errorf("Error sending request: %v", err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Printf("Response Status: %s\n", resp.Status)
+    fmt.Printf("Response Status Code: %d\n", resp.StatusCode)
+    return nil
+}
+
 func main() {
-	config, err:= parseFlags()
-	flag.Parse()
-	if err != nil{
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Printf("URL provided:%s\n", config.URL)
+    config, err := parseFlags()
+    if err != nil {
+        fmt.Println(err)
+        flag.Usage()
+        os.Exit(1)
+    }
+
+    fmt.Printf("Making request to: %s\n\n", config.URL)
+
+    if err := makeRequest(config); err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
 }
